e5a5ea6d84d7f2b6665f408199755377d74db72e
Quy Dao Xuan <quydx@MacBook-Pro.local>
2026-03-19 00:33:07 +0700

    feat: add option

--- app/constants.ts
+++ app/constants.ts
@@ -1,46 +1,51 @@
 export const CLIENTS = [
-  {
-    name: "DSM-F",
-    value: "dsm-f",
-  },
-  {
-    name: "Ferrero",
-    value: "ferrero",
-  },
-  {
-    name: "Ferrero Chocolate",
-    value: "ferrero-chocolate",
-  },
-  {
-    name: "Ferrero New",
-    value: "ferrero-new",
-  },
-  {
-    name: "Kerry",
-    value: "kerry",
-  },
-  {
-    name: "Kerry Requirements",
-    value: "kerry-requirements",
-  },
-  {
-    name: "Croda",
-    value: "croda",
-  },
-  {
-    name: "Kalbe",
-    value: "kalbe",
-  },
-  {
-    name: "Green Pharma",
-    value: "green-pharma",
-  },
-  {
-    name: "Bayer",
-    value: "bayer",
-  },
-] as const;
+	{
+		name: "GTC",
+		value: "gtc",
+	},
+	{
+		name: "DSM-F",
+		value: "dsm-f",
+	},
+	{
+		name: "Ferrero",
+		value: "ferrero",
+	},
+	{
+		name: "Ferrero Chocolate",
+		value: "ferrero-chocolate",
+	},
+	{
+		name: "Ferrero New",
+		value: "ferrero-new",
+	},
+	{
+		name: "Kerry",
+		value: "kerry",
+	},
+	{
+		name: "Kerry Requirements",
+		value: "kerry-requirements",
+	},
+	{
+		name: "Croda",
+		value: "croda",
+	},
+	{
+		name: "Kalbe",
+		value: "kalbe",
+	},
+	{
+		name: "Green Pharma",
+		value: "green-pharma",
+	},
+	{
+		name: "Bayer",
+		value: "bayer",
+	},
+] as const
 
-export const VALID_CLIENTS = CLIENTS.map((c) => c.value) as string[];
+export const VALID_CLIENTS = CLIENTS.map((c) => c.value) as string[]
 
-export type ClientId = (typeof CLIENTS)[number]["value"];
+export type ClientId = (typeof CLIENTS)[number]["value"]
+
