cf92c6c7c084998836c91df29260593b9dd944c8
Quy Dao Xuan <quydx@MacBook-Pro.local>
2026-03-31 21:29:41 +0700

    feat: gfc demo

--- app/constants.ts
+++ app/constants.ts
@@ -1,55 +1,54 @@
 export const CLIENTS = [
-  {
-    name: "GTC",
-    value: "gtc",
-  },
-  {
-    name: "DSM-F",
-    value: "dsm-f",
-  },
-  {
-    name: "Ferrero",
-    value: "ferrero",
-  },
-  {
-    name: "Ferrero Chocolate",
-    value: "ferrero-chocolate",
-  },
-  {
-    name: "Ferrero New",
-    value: "ferrero-new",
-  },
-  {
-    name: "Kerry",
-    value: "kerry",
-  },
-  {
-    name: "Kerry Requirements",
-    value: "kerry-requirements",
-  },
-  {
-    name: "Croda",
-    value: "croda",
-  },
-  {
-    name: "Kalbe",
-    value: "kalbe",
-  },
-  {
-    name: "Green Pharma",
-    value: "green-pharma",
-  },
-  {
-    name: "China Demo",
-    value: "china-demo",
-  },
-  {
-    name: "GSK",
-    value: "gsk",
-  }
-] as const;
+	{
+		name: "GFR",
+		value: "gfr",
+	},
+	{
+		name: "GTC",
+		value: "gtc",
+	},
+	{
+		name: "DSM-F",
+		value: "dsm-f",
+	},
+	{
+		name: "Ferrero",
+		value: "ferrero",
+	},
+	{
+		name: "Ferrero Chocolate",
+		value: "ferrero-chocolate",
+	},
+	{
+		name: "Ferrero New",
+		value: "ferrero-new",
+	},
+	{
+		name: "Kerry",
+		value: "kerry",
+	},
+	{
+		name: "Kerry Requirements",
+		value: "kerry-requirements",
+	},
+	{
+		name: "Croda",
+		value: "croda",
+	},
+	{
+		name: "Kalbe",
+		value: "kalbe",
+	},
+	{
+		name: "Green Pharma",
+		value: "green-pharma",
+	},
+	{
+		name: "Bayer",
+		value: "bayer",
+	},
+] as const
 
 export const VALID_CLIENTS = CLIENTS.map((c) => c.value) as string[]
 
 export type ClientId = (typeof CLIENTS)[number]["value"]
-
